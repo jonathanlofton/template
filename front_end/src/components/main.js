781c57327c3cfb2fae8a6721a7853e8b11c8ce04
Jonathan Lofton <jonathanlofton91@gmail.com>
2024-11-30 21:56:01 -0700

    added bootstrap, removed tailwind, styled the UI a bit

--- front_end/src/components/main.js
+++ front_end/src/components/main.js
@@ -3,10 +3,17 @@ import Users from './users/users';
 
 const Main = () => {
   return (
-    <div>
-      HELLO THIS IS THE BODY
-
-      <Users/>
+    <div className="yup">
+      <div className='header red-border'>
+        Hello this is the header
+      </div>
+      <div className='body red-border'>
+        HELLO THIS IS THE BODY
+        <Users/>
+      </div>
+      <div className='footer red-border'>
+        Hello this is the footer
+      </div>
     </div>
   );
 }
